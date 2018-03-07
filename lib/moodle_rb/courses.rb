131aea2b374351285145e099f254ad90cef720ca
Mauricio Krzesinsk Junior <mauriciokj@gmail.com>
2018-03-07 14:06:13 -0300

    add extra params to course creation

--- lib/moodle_rb/courses.rb
+++ lib/moodle_rb/courses.rb
@@ -42,7 +42,9 @@ module MoodleRb
                 :fullname => params[:full_name],
                 :shortname => params[:short_name],
                 :categoryid => params[:parent_category],
-                :idnumber => params[:idnumber]
+                :idnumber => params[:idnumber],
+                :startdate => params[:startdate],
+                :enddate => params[:enddate]
               }
             }
           }
